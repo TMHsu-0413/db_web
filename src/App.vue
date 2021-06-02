439f5ed6ebcc2279860e3a8c998ec57f84222410
TangMing <40743235@gm.nfu.edu.tw>
2021-06-02 20:10:14 +0800

    字體更改

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <router-view/>
+  <div id="app">
+    <router-view/>
+  </div>
 </template>
 
 <script>
@@ -11,12 +13,12 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@500&display=swap');
+@import url('https://fonts.googleapis.com/css?family=Noto+Sans+TC');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'JetBrains Mono',"Noto Sans TC",sans-serif;
+  font-weight: 400;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
