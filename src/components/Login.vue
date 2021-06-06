6b00c68cdbb3700bd74ede555253115549206aed
TangMing <40743235@gm.nfu.edu.tw>
2021-06-06 14:13:26 +0800

    修改登入視窗

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,16 +1,20 @@
 <template>
+  <div class="flex">
     <div class="form">
       <div class="close"><button class="closebtn" @click='CloseLogin'>&times;</button></div>
-      <div class="type_block">
-        <input type="text" v-model="Account" class="input" placeholder="帳號" />
-        <h2 class="label">帳號</h2>
-      </div>
-      <div class="type_block">
-        <input type="text" v-model="Password" class="input" placeholder="密碼" />
-        <h2 class="label">密碼</h2>
+      <div class="flex-col">
+        <div class="type_block">
+          <input type="text" v-model="Account" class="input" placeholder="帳號" />
+          <h2 class="label">帳號</h2>
+        </div>
+        <div class="type_block">
+          <input type="text" v-model="Password" class="input" placeholder="密碼" />
+          <h2 class="label">密碼</h2>
+        </div>
       </div>
       <button class="btn">登入</button>
     </div>
+  </div>
 </template>
 
 <script>
@@ -28,28 +32,40 @@ export default {
 </script>
 
 <style scoped>
+@keyframes fadein{
+  from {opacity: 0; transform: translateY(100px);}
+  to {opacity: 1; transform: translate(0);}
+}
+.flex{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 .form{
   position: absolute;
   padding:2rem 1rem;
   border-radius: 10px;
-  top:50%;
-  left:50%;
-  transform: translate(-50%,-50%);
-  width:40vw;
-  height: 25vh;
-  background-color: rgba(255,255,255,.4);
-  border: 2px solid;
-  border-image: linear-gradient(to bottom,rgba(255,255,255,.5),rgba(0,0,255,.5));
+  width:18vw;
+  height: 35vh;
+  background-color: rgba(255,255,255,.45);
+  border: 2px solid rgba(17, 223, 238, 0.4);
   border-image-slice: 1;
   display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
+  flex-direction: column;
+  animation: fadein 1s;
+}
+.flex-col{
+  display: flex;
+  flex-direction: column;
 }
 .type_block{
   flex-basis: 50%;
   display: flex;
   position: relative;
   flex-direction: column;
+}
+.type_block:not(:last-child){
+  margin-bottom: 3rem;
 }
 .input{
   padding:.5rem .5rem;
@@ -60,9 +76,7 @@ export default {
   transition: all .3s;
 }
 .input:focus{
-  transform: scalex(1.05) scaleY(1.03);
   outline: none;
-  border: 3px solid rgba(0,0,255,.5);
 }
 .input:-ms-input-placeholder{
   color:gray;
@@ -70,26 +84,26 @@ export default {
 .input:placeholder-shown + .label{
   opacity: 0;
   visibility: hidden;
-  transform: translateY(-4px);
+  transform: translateY(4px);
 }
 .label{
   color:rgba(0,0,255,.7);
   position: absolute;
   left:2.5rem;
-  top:3.5rem;
+  top:-2rem;
   font-size: 1.5rem;
   transition: all .3s;
 }
 .btn{
   background-color: rgba(0,0,255,.4);
-  margin-top:4rem;
+  margin-top:3rem;
   color: white;
-  width:50%;
+  width:80%;
   outline:0;
   padding:.5rem 1rem;
   border-radius: 20px;
   transition: all .2s;
-  flex-basis: 40%;
+  align-self: center;
 }
 .btn:hover{
   transform: translateY(-3px);
@@ -102,7 +116,7 @@ export default {
   border: none;
 }
 .close{
-  flex-basis: 100%;
+  margin-bottom: 2rem;
 }
 .closebtn{
   font-size: 1.5rem;
