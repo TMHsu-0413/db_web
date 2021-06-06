805bee3d35f43b64d596896cf50892cf84ee2467
TangMing <40743235@gm.nfu.edu.tw>
2021-06-06 14:36:57 +0800

    修改註冊畫面視窗

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,102 +1,111 @@
 <template>
-  <div>
-    <div class="bg"></div>
-    <button @click="CloseRegister"></button>
+  <div class="flex">
     <div class="form">
-      <div class="type_block">
-        <input type="text" v-model="Account" class="input" placeholder="帳號" />
-        <h2 class="label">帳號</h2>
+      <div class="close"><button class="closebtn" @click='CloseRegister'>&times;</button></div>
+      <div class="flex-row">
+        <div class="type_block fbasis50">
+          <input type="text" v-model="Account" class="input w80" placeholder="帳號" />
+          <h2 class="label">帳號</h2>
+        </div>
+        <div class="type_block fbasis50">
+          <input type="text" v-model="Name" class="input w80" placeholder="用戶姓名" />
+          <h2 class="label">用戶姓名</h2>
+        </div>
+        <div class="type_block fbasis50">
+          <input type="password" v-model="Password" class="input w80" placeholder="密碼" />
+          <h2 class="label">密碼</h2>
+        </div>
+        <div class="type_block fbasis50">
+          <input type="password" v-model="ConPassword" class="input w80" placeholder="再輸入一次密碼" />
+        </div>
+        <div class="type_block fbasis50">
+          <input type="text" v-model="Phone" class="input w80" placeholder="聯絡電話" />
+          <h2 class="label">聯絡電話</h2>
+        </div>
+        <div class="type_block fbasis50">
+          <input type="text" v-model="Email" class="input w80" placeholder="E-mail" />
+          <h2 class="label">E-mail</h2>
+        </div>
+        <div class="type_block fbasis100">
+          <input type="text" v-model="Address" class="input w90" placeholder="聯絡地址" />
+          <h2 class="label">聯絡地址</h2>
+        </div>
       </div>
-      <div class="type_block">
-        <input type="password" v-model="Password" class="input" placeholder="密碼" />
-        <h2 class="label">密碼</h2>
-      </div>
-      <div class="type_block">
-        <input type="password" v-model="ConfirmPassword" class="input" placeholder="再輸入一次密碼" />
-        <h2 class="label">確認密碼</h2>
-      </div>
-      <div class="type_block">
-        <input type="text" v-model="UserName" class="input" placeholder="用戶姓名" />
-        <h2 class="label">用戶姓名</h2>
-      </div>
-      <div class="type_block">
-        <input type="text" v-model="Address" class="input" placeholder="聯絡地址" />
-        <h2 class="label">聯絡地址</h2>
-      </div>
-      <div class="type_block">
-        <input type="text" v-model="Phone" class="input" placeholder="連絡電話" />
-        <h2 class="label">連絡電話</h2>
-      </div>
-      <div class="type_block">
-        <input type="text" v-model="Mail" class="input" placeholder="E-mail" />
-        <h2 class="label">E-mail</h2>
-      </div>
-      <button class="btn">註冊</button>
+      <button class="btn">登入</button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  props: {
+    CloseRegister: Function
+  },
   data () {
     return {
       Account: null,
-      Password: null,
-      ConfirmPassword: null,
-      UserName: null,
-      Address: null,
-      Phone: null,
-      Mail: null
+      Password: null
     }
-  },
-  props: {
-    CloseRegister: Function
   }
 }
 </script>
 
 <style scoped>
-.bg{
-  position: relative;
-  background-image: linear-gradient(rgba(0,0,255,0.5),rgba(255,255,255,0.5)),url('../image/bg-4.jpg');
-  filter: blur(1px);
-  background-size: cover;
-  height: 100vh;
+@keyframes fadein{
+  from {opacity: 0; transform: translateY(100px);}
+  to {opacity: 1; transform: translate(0);}
+}
+.flex{
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 .form{
   position: absolute;
   padding:2rem 1rem;
   border-radius: 10px;
-  top:50%;
-  left:50%;
-  transform: translate(-50%,-50%);
-  width: 30vw;
-  height: 80vh;
-  background-color: rgba(255,255,255,.4);
-  border: 2px solid;
-  border-image: linear-gradient(to bottom,rgba(255,255,255,.5),rgba(0,0,255,.5));
+  width:30vw;
+  height: 50vh;
+  background-color: rgba(255,255,255,.45);
+  border: 2px solid rgba(17, 223, 238, 0.4);
   border-image-slice: 1;
   display: flex;
   flex-direction: column;
+  animation: fadein 1s;
+}
+.flex-row{
+  display: flex;
+  flex-wrap: wrap;
+}
+.fbasis50{
+  flex-basis:50%;
+}
+.fbasis100{
+  flex-basis: 100%;
 }
 .type_block{
-  flex-basis: 40%;
   display: flex;
   position: relative;
-  flex-direction: column;
+  justify-content: center;
+}
+.type_block:not(:last-child){
+  margin-bottom: 3rem;
 }
 .input{
   padding:.5rem .5rem;
-  width:80%;
   align-self: center;
   border: none;
   border-radius: 5px;
   transition: all .3s;
 }
+.w80{
+  width:80%;
+}
+.w90{
+  width:90%;
+}
 .input:focus{
-  transform: scalex(1.05) scaleY(1.03);
   outline: none;
-  border: 3px solid rgba(0,0,255,.5);
 }
 .input:-ms-input-placeholder{
   color:gray;
@@ -104,25 +113,26 @@ export default {
 .input:placeholder-shown + .label{
   opacity: 0;
   visibility: hidden;
-  transform: translateY(-4px);
+  transform: translateY(4px);
 }
 .label{
-  color:rgba(120, 3, 150, 0.7);
+  color:rgba(0,0,255,.7);
   position: absolute;
-  left:3.8rem;
-  top:2.7rem;
-  font-size: 1rem;
+  left:2.5rem;
+  top:-2rem;
+  font-size: 1.2rem;
   transition: all .3s;
 }
 .btn{
-  background-color: rgba(123, 4, 202, 0.4);
+  background-color: rgba(0,0,255,.4);
+  margin-top:3rem;
   color: white;
-  width:50%;
+  width:60%;
   outline:0;
-  align-self: center;
   padding:.5rem 1rem;
   border-radius: 20px;
   transition: all .2s;
+  align-self: center;
 }
 .btn:hover{
   transform: translateY(-3px);
@@ -134,5 +144,15 @@ export default {
   outline: none;
   border: none;
 }
+.close{
+  margin-bottom: 2rem;
+}
+.closebtn{
+  font-size: 1.5rem;
+  background-color: transparent;
+  outline: none;
+  border: none;
+  margin-top:-1rem;
+  float: right;
+}
 </style>
-
