6c25f90a61cecb3d1dc06b380af0937d1972dbc0
Ren <40743234@gm.nfu.edu.tw>
2021-06-07 15:20:57 +0800

    增加上傳貼文頁面(RWD)

--- src/Views/UploadPost.vue
+++ src/Views/UploadPost.vue
@@ -1,16 +1,52 @@
 <template>
         <div class="bg">
             <div class="form">
-                <div class="content">
-                    <div class="row mt-2 mb-2">
-                        <input type="text" class="form-control" placeholder="ItemName" aria-label="ItemName">
+                <div class="container">
+                    <div class="row mt-2 mb-2 px-5">
+                      <input type="text" class="form-control" placeholder="ItemName" aria-label="ItemName">
                     </div>
-                    <div class="row mt-2 mb-2">
-                        <input type="text" class="form-control" placeholder="ItemQuanitiy" aria-label="ItemQuanitiy" >
+                    <div class="row mt-2 mb-2 px-5">
+                      <input type="text" class="form-control" placeholder="ItemQuanitiy" aria-label="ItemQuanitiy" >
                     </div>
-                    <div class="row mt-2 mb-2">
-                        <input type="file" class="form-control" placeholder="ItemPicutre" aria-label="ItemQuanitiy" >
+                    <div class="row mt-2 mb-2 px-5">
+                      <input type="file" class="form-control" placeholder="ItemPicutre" aria-label="ItemPicutre" >
                     </div>
+                    <div class="row mt-2 mb-2 px-5">
+                      <input type="text" class="form-control" placeholder="ItemAddress" aria-label="ItemAddress" >
+                    </div>
+                    <div class="row mt-2 mb-2 px-5">
+                      <textarea class="form-control" placeholder="ItemSituation" aria-label="ItemSituation"></textarea>
+                    </div>
+                    <h5 class="row mt-2 mb-2 px-5">若是超過時限無人交換,則:</h5>
+                    <div class="row mt-2 mb-2 px-5">
+                      <div class="col">
+                        <div class="form-check form-check-inline">
+                          <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+                          <label class="form-check-label" for="inlineRadio1">捐贈至該系統</label>
+                        </div>
+                      </div>
+                      <div class="col">
+                        <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+                        <label class="form-check-label" for="inlineRadio2">自行收回</label>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="row mt-2 mb-2 px-5">
+                      <input type="text" class="form-control" placeholder="WantItemName" aria-label="WantItemName">
+                    </div>
+                    <div class="row mt-2 mb-2 px-5">
+                      <textarea class="form-control" placeholder="WantItemSituation" aria-label="WantItemSituation"></textarea>
+                    </div>
+                </div>
+                <div class = "container">
+                  <div class="row">
+                    <div class="col"></div>
+                    <div class="col"></div>
+                    <div class="col">
+                     <button type="button" class="btn btn-dark">上傳貼文</button>
+                    </div>
+                  </div>
                 </div>
             </div>
         </div>
@@ -30,10 +66,10 @@
 }
 .form{
   position: absolute;
-  padding:2rem 1rem;
+  padding:2rem 0rem;
   border-radius: 10px;
-  width:30vw;
-  height: 58vh;
+  /* width:30vw;
+  height: 80vh; */
   background-color: rgba(255,255,255,.45);
   border: 2px solid rgba(17, 223, 238, 0.4);
   border-image-slice: 1;
